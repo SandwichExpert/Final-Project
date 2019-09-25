e099707dc79cf62ced12cf572e26cfd9098d155a
Xavier Crespo <xavier.crespo1@gmail.com>
2019-09-25 15:14:13 +0200

    Added user deletion from meetup

--- server/routes/MeetUps.js
+++ server/routes/MeetUps.js
@@ -1,87 +1,109 @@
-const express = require('express');
-const MeetUp = require('../models/MeetUp');
-const {isLoggedIn} = require('../middlewares'); 
-const router = express.Router();
+const express = require('express')
+const MeetUp = require('../models/MeetUp')
+const { isLoggedIn } = require('../middlewares')
+const router = express.Router()
 
-router.get('/my-meetups', isLoggedIn, (req,res,next)=>{
+router.get('/my-meetups', isLoggedIn, (req, res, next) => {
   MeetUp.find()
-  .populate("_admin")
-  .populate("_users")
-  .then(meetup =>{
-    res.json(meetup)
-  })
+    .populate('_admin')
+    .populate('_users')
+    .then(meetup => {
+      res.json(meetup)
+    })
 })
 
-router.post('/meetups',isLoggedIn,(req,res,next)=>{
-  const _admin = req.user._id;
-  const meetup_date = req.body.meetup_date;
-  const meetup_time = req.body.meetup_time;
-  const name = req.body.name;
+router.post('/meetups', isLoggedIn, (req, res, next) => {
+  const _admin = req.user._id
+  const meetup_date = req.body.meetup_date
+  const meetup_time = req.body.meetup_time
+  const name = req.body.name
 
-  const newMeetUp={
+  const newMeetUp = {
     _admin,
     meetup_date,
     meetup_time,
-    name
+    name,
   }
-  
-  MeetUp.create(newMeetUp)
-  .then(meetup=>{
+
+  MeetUp.create(newMeetUp).then(meetup => {
     res.json(meetup)
   })
-
 })
 
 router.post('/:meetupId', isLoggedIn, (req, res, next) => {
-  const options = {new:true}
+  const options = { new: true }
   const meetup = req.params.meetupId
-  MeetUp.findByIdAndUpdate(meetup, { $push: { _users: req.user } },options)
-  .then(updatedMeetup =>{
-    res.json(updatedMeetup);
+
+  MeetUp.findByIdAndUpdate(
+    meetup,
+    { $push: { _users: req.user } },
+    options
+  ).then(updatedMeetup => {
+    res.json(updatedMeetup)
   })
 })
 
-// router.delete('/:meetupId/:meetupUserId', isIdentified, (req, res, next) => {
-//   const options = {new:true};
-//   const meetup = req.params.meetupId;
-//   const meetupUser = req.params.meetupUserId;
-//   const adminId = req.params.admin
+router.delete('/:meetupId/:meetupUserId', isLoggedIn, (req, res, next) => {
+  const options = { new: true };
+  const meetupId = req.params.meetupId;
+  const meetupUserId = req.params.meetupUserId;
+  const currentUserId = req.user._id;
+  console.log('-----', meetupUserId, '-----------', meetupId);
+  findAdminFromMeetup(meetupId, meetupUserId, currentUserId).then(updatedMeetup => {
+    res.json(updatedMeetup)
+  })
 
-//   MeetUp.findById(meetupUser)
-//   .then(deletedUser => {
-//     if(!deletedUser){
-//       next({success:false, status : 400})
-//     }
-//     if (adminId == meetup.admin.toString() || deletedUser.id == meetupUser){
-//       MeetUp.findByIdAndDelete(deletedUser._id).then(()=>{
-//         res.json({success:true})
-//       })
-//     }
-//     else res.json({success : false, status : 403})
-//   })
-//   MeetUp.findByIdAndDelete(meetup, meetupUser,options)
-//   .then(updatedMeetup =>{
-//     res.json(updatedMeetup);
-//   })
-// })
+  // MeetUp.findById(meetupUser)
+  // .then(deletedUser => {
+  //   const adminId = meetup._admin
+  //   if(!deletedUser){
+  //     next({success:false, status : 400})
+  //   }
+  //   if (adminId == meetup.admin.toString() || deletedUser.id == meetupUser){
+  //     MeetUp.findByIdAndDelete(deletedUser._id).then(()=>{
+  //       res.json({success:true})
+  //     })
+  //   }
+  //   else res.json({success : false, status : 403})
+  // })
+  // MeetUp.findByIdAndDelete(meetup, meetupUser,options)
+  // .then(updatedMeetup =>{
+  //   res.json(updatedMeetup);
+  // })
+})
 
-router.delete('meetups/:meetupId',isLoggedIn,(req,res,next)=>{
-  let adminId = req.user._id.toString();
+router.delete('/:meetupId', isLoggedIn, (req, res, next) => {
+  let adminId = req.user._id.toString()
   MeetUp.findById(req.params.meetupId)
-  .then(deletedMeetUp =>{
-    if(!deletedMeetUp)
-    {
-      next({success:false,status : 400})
-    }
-    if(adminId == deletedMeetUp._admin.toString()){
-      MeetUp.findByIdAndDelete(deletedMeetUp._id)
-      .then(()=>res.json({success:true}))
-    }
-    else{
-      res.json({success:false})
-    }
-  })
-  .catch(err => next(err))
+    .then(deletedMeetUp => {
+      if (!deletedMeetUp) {
+        next({ success: false, status: 400 })
+      }
+      if (adminId == deletedMeetUp._admin.toString()) {
+        MeetUp.findByIdAndDelete(deletedMeetUp._id).then(() =>
+          res.json({ success: true })
+        )
+      } else {
+        res.json({ success: false })
+      }
+    })
+    .catch(err => next(err))
 })
+
+async function findAdminFromMeetup(meetupId, meetupUserId, currentUserId) {
+  const meetup = await MeetUp.findById(meetupId)
+  const admin = meetup._admin
+  console.log(admin,"-------------YO-----------")
+  if (currentUserId == admin || currentUserId == meetupUserId) {
+    console.log(currentUserId,"-------------YO-----------")
+    const updatedMeetup = await MeetUp.findByIdAndUpdate(
+      meetupId,
+      { $pull: { _users: meetupUserId } },
+      { new: true }
+    )
+    console.log(updatedMeetup,"YAAAAAAAAAAAAAAAAAAAAAAA")
+    return updatedMeetup
+  }
+}
 
 module.exports = router
