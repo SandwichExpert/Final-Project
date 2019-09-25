d40b25069bd1598e9a96ed838d8e5896a2e01638
belke05 <you@example.com>
2019-09-25 12:32:28 +0200

    user edit delete and fetch

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,38 +6,68 @@ const uploader = require('../configs/cloudinary')
 router
   .get('/:userid', (req, res, next) => {
     const id = req.params.userid
-    findUser(id).then(user => {
-      console.log('user requested')
-      res.json(user)
-    })
+    findUser(id)
+      .then(user => {
+        console.log('user requested', user)
+        if (user) {
+          user.password = undefined
+          res.json(user)
+        } else {
+          res.json({ msg: 'user not found' })
+        }
+      })
+      .catch(err => console.log(err))
   })
   .delete('/:userid', (req, res, next) => {
     const id = req.params.userid
-    deleteUser(id).then(user => {
-      console.log('user deleted')
-      res.json(user)
-    })
+    deleteUser(id)
+      .then(user => {
+        console.log('user deleted')
+        user.password = undefined
+        res.json(user)
+      })
+      .catch(err => console.log(err))
   })
-  .post('/edit', uploader.single('avatar'), (req, res, next) => {
-    deleteUser(id).then(user => {
-      console.log('user deleted')
-      res.json(user)
-    })
+  .put('/edit/:userid', uploader.single('avatar'), (req, res, next) => {
+    let changes = req.body
+    req.file ? (changes.avatar = req.file.url) : null
+    const id = req.params.userid
+    console.log(req.body)
+    updateUser(id, changes)
+      .then(user => {
+        console.log('user updated')
+        user.password = undefined
+        res.json(user)
+      })
+      .catch(err => console.log(err))
   })
 
 async function findUser(id) {
-  const foundUser = await User.findById(id)
-  return foundUser
+  try {
+    const foundUser = await User.findById(id)
+    return foundUser
+  } catch (err) {
+    return res.json({ msg: 'user not found' })
+  }
 }
 
 async function deleteUser(id) {
-  const deletedUser = await User.findByIdAndDelete(id)
-  return deletedUser
+  try {
+    const deletedUser = await User.findByIdAndDelete(id)
+    return deletedUser
+  } catch (err) {
+    return alert(err)
+  }
 }
 
-async function updateUser(id) {
-  const updateUser = await User.findByIdAndUpdate(id)
-  return updatedUser
+async function updateUser(id, changes) {
+  const options = { new: true }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(id, changes, options)
+    return updatedUser
+  } catch (err) {
+    return alert(err)
+  }
 }
 
 module.exports = router
