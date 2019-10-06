9756ce4acfaf03192fcce8130d6afb468315a66a
Xavier Crespo <xavier.crespo1@gmail.com>
2019-10-06 23:10:13 +0200

    added chat functions and toggle

---
+++ client/src/components/sub-components/Store.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import io from "socket.io-client";
+export const CTX = React.createContext();
+
+const initState = {
+  general: [{ from: "maptee", msg: "Hello there" }]
+};
+
+function reducer(state, action) {
+  const { from, msg, topic } = action.payload;
+  switch (action.type) {
+    case "RECEIVE_MESSAGE":
+      return {
+        ...state,
+        [topic]: [...state[topic], { from, msg }]
+      };
+    default:
+      return state;
+  }
+}
+
+let socket;
+
+function sendChatAction(value) {
+  socket.emit("chat message", value);
+}
+
+export default function Store(props) {
+  const [allChats, dispatch] = React.useReducer(reducer, initState);
+  if (!socket) {
+    socket = io(":3002");
+    socket.on("chat message", function(msg) {
+      dispatch({ type: "RECEIVE_MESSAGE", payload: msg });
+    });
+  }
+  const user = props.user.first_name;
+  const avatar = props.user.avatar;
+
+  return (
+    <CTX.Provider value={{ allChats, sendChatAction, user, avatar }}>
+      {props.children}
+    </CTX.Provider>
+  );
+}
