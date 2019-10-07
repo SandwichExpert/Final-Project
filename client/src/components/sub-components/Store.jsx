7530737a5fda6716552c354cd42eda49748eddc8
Xavier Crespo <xavier.crespo1@gmail.com>
2019-10-07 12:29:12 +0200

    updated socket

--- client/src/components/sub-components/Store.jsx
+++ client/src/components/sub-components/Store.jsx
@@ -28,7 +28,7 @@ function sendChatAction(value) {
 export default function Store(props) {
   const [allChats, dispatch] = React.useReducer(reducer, initState);
   if (!socket) {
-    socket = io(":443/");
+    socket = io("http://maptee.herokuapp.com/");
     socket.on("chat message", function(msg) {
       dispatch({ type: "RECEIVE_MESSAGE", payload: msg });
     });
