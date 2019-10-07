30df0fd6ab7ca8308bf6730528c26fdc5bf0b9f4
belke05 <you@example.com>
2019-10-07 10:56:52 +0200

    resolve conflict

--- server/app.js
+++ server/app.js
@@ -10,11 +10,6 @@ const logger = require("morgan");
 const nocache = require("nocache");
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
-// const http = require('http').createServer(app);
-// var io = require('socket.io')(http);
-
-
-
 require("./configs/database");
 
 const app_name = require("./package.json").name;
@@ -26,11 +21,6 @@ const debug = require("debug")(
 //   socket.on
 //   console.log('a user connected')
 // });
-
-// io.on('connection',(socket)=>{
-//   console.log('Client connected');
-//   socket.on('disconnect', () => console.log('Client disconnected'));
-// })
 
 app.use(nocache());
 
